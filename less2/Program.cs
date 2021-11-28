7fdedf530c8658bd6f1e05d357517ba8c3ee8640
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 16:09:18 +0500

    Добавьте файлы проекта.

---
+++ less2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace less2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int totalPictures = 52;
+            int numberOfRow = totalPictures /picturesInRow;
+            int restOfPictures = totalPictures % picturesInRow;
+            Console.WriteLine(numberOfRow);
+            Console.WriteLine(restOfPictures);
+        }
+    }
+}
